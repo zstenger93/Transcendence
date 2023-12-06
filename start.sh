a8192f7e50f20ccd8b995ab6524684434464d883
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-06 14:33:18 +0100

    test for venv

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /app/TranscEND
+apt install python3.11-venv -y
+python3 -m venv venv
 source venv/bin/activate
+pip install --no-cache-dir django
+echo 'source /app/venv/bin/activate' >> /root/.bashrc
 python3 manage.py runserver 0.0.0.0:8000
