b8779145d01b06a1005788d17be5788b413061f4
AzerSD <sioudazer8@gmail.com>
2023-12-10 01:15:03 +0100

    added environment to docker-compose changed compose port to 8000 instead of 8080 for now Now we're able to authenticate users and save sessions added postgresql to docker-compose changed db to postgresql in djangoj

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 # Create different uri roots to connect them to a view 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("login/", views.login, name="login"),
+    path('logout/', views.logout, name='logout'),
     path("auth/", views.auth, name="auth"),
     path("auth_callback/", views.auth_callback, name="auth_callback"),
-    path("", views.home, name="home"),
 ]
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcend_users_db',
+        'USER': 'transcend_user',
+        'PASSWORD': 'transcend_pwd',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
