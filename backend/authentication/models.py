3ae37063767f3cea7c3a339b141ff8d10b7a1320
AzerSD <sioudazer8@gmail.com>
2023-12-10 06:54:16 +0100

    basic user done; starting user database management

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    picture = models.URLField()
