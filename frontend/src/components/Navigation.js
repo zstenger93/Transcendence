24f6dac6edb367de92b5cf9741ddc93a9d3d00e7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-13 01:37:53 +0100

    frontend setup

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Welcome</Link></li>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex">
+        <li className="mr-6">
+          <Link to="/">Welcome</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
