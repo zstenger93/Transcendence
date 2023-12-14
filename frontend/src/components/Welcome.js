817bb770aea14f7411a1bdd2e913342637529859
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-14 18:08:12 +0100

    more like ffs not css

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -11,15 +11,15 @@ function Welcome() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="text-center mb-8">
+      {/* <div className="text-center mb-8">
         <h1 className="text-6xl text-white font-bold mb-4">
           Welcome to<br />Transcendence
         </h1>
         <h1 className="text-6xl text-white font-bold mb-8">
           Where the<br />Core journey<br /> Ends
         </h1>
-      </div>
-      <div className='flex items-center justify-center h-screen'>
+      </div> */}
+      <div className=' flex items-center justify-center h-screen'>
         <button
           className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-red-700"
           onClick={redirectToHome}
