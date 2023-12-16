edaf4d92493463eeff1d0b4f1fd00a848e99999f
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-16 12:39:42 +0100

    creating starting point for game so karlis can work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import Home from './components/Home';
+import Chat from './components/Chat';
+import Games from './components/Games';
 import About from './components/About';
+import Sidebar from './components/Sidebar';
 import Welcome from './components/Welcome';
 import Profile from './components/Profile';
 import Matchmaking from './components/Matchmaking';
-import Chat from './components/Chat';
-import Sidebar from './components/Sidebar';
 import backgroundImage from './images/welcome.jpg';
+import OriginalPong from './components/OriginalPong';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
@@ -18,15 +19,21 @@ function App() {
 				<Route
 					path="/*"
 					element={
-						<div className="bg-cover bg-center h-screen w-full" style={{ backgroundImage: `url(${backgroundImage})` }}>
+						<div className="bg-cover bg-center h-screen w-full" style={{
+							backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover',
+							backgroundPosition: 'center',
+							backgroundRepeat: 'no-repeat'
+						}}>
 							<Sidebar />
 							<Routes>
+								<Route path="/" element={<Navigate to="/home" />} />
 								<Route path="home" element={<Home />} />
 								<Route path="chat" element={<Chat />} />
 								<Route path="matchmaking" element={<Matchmaking />} />
-								<Route path="about" element={<About />} />
+								<Route path="games" element={<Games />} />
 								<Route path="profile" element={<Profile />} />
-								<Route path="/" element={<Navigate to="/home" />} />
+								<Route path="about" element={<About />} />
+								<Route path="originalpong" element={<OriginalPong />} />
 							</Routes>
 						</div>
 					}
--- frontend/src/components/WelcomeMessage.js
+++ frontend/src/components/WelcomeMessage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Navigation() {
+function WelcomeMessage() {
     return (
         <nav className="bg-opacity-0 p-2 text-center">
             <span className="text-white font-extrabold text-4xl">Welcome To<br />TranscENDence<br />Where Your Core Journey<br />Ends</span>
@@ -8,4 +8,4 @@ function Navigation() {
     );
 }
 
-export default Navigation;
+export default WelcomeMessage;
