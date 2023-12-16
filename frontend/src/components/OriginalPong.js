5b95327e8c3c1115808c19b79163af88202ff3c0
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-16 13:16:31 +0100

    init branch for karlis

--- frontend/src/components/OriginalPong.js
+++ frontend/src/components/OriginalPong.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import backgroundImage from '../images/welcome.jpg';
 
+// page for the original pong game
+
 function OriginalPong() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
