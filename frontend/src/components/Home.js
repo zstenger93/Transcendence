1f38eea9f06f0a6de6232835c7b94374fe542868
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-18 10:32:09 +0100

    mostly background and color style changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import backgroundImage from '../images/welcome.jpg';
+import backgroundImage from '../images/bg0.png';
 import WelcomeMessage from './WelcomeMessage';
 
 function Home() {
-    return (
-        <div className="flex flex-col h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <WelcomeMessage />
-        </div>
-    );
+	return (
+		<div className="flex flex-col h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+			<WelcomeMessage />
+		</div>
+	);
 }
 
 export default Home;
