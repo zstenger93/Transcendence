46b47244536dc34a7fd32ddbb2ca82c92ac8ea34
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-18 14:02:44 +0100

    formatter

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import backgroundImage from '../images/bg0.png';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import backgroundImage from "../images/bg0.png";
 
 function Welcome() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const redirectToHome = () => {
-		navigate('/home');
-	};
+  const redirectToHome = () => {
+    navigate("/home");
+  };
 
-	return (
-		<div className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
-			<div className=' flex items-center justify-center h-screen'>
-				<button
-					className="bg-gray-900 text-gray-300 font-nosifer font-bold px-4 py-2 rounded cursor-pointer hover:bg-gray-900 hover:bg-opacity-70"
-					onClick={redirectToHome}
-				>
-					Sign In
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className=" flex items-center justify-center h-screen">
+        <button
+          className="bg-gray-900 text-gray-300 font-nosifer font-bold px-4 py-2 rounded cursor-pointer hover:bg-gray-900 hover:bg-opacity-70"
+          onClick={redirectToHome}
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
