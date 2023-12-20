900e62a982bf191f071d994ab6630c2bc6ca028c
Jamshidbek Ergashev <jergashe@student.42heilbronn.de>
2023-12-20 11:26:27 +0100

    User has new field: profile_picture. We can save pictures

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -4,5 +4,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+        
+    profile_picture = models.ImageField(upload_to='authentication/images/', null=True, blank=True, default='default_profile_picture.PNG')
+    total_matches = models.PositiveIntegerField(default=0)
+    wins = models.PositiveIntegerField(default=0)
+    losses = models.PositiveIntegerField(default=0)
+
+
     def __str__(self) -> str:
-        return "Custom User: " + super().__str__()
+        return "Custom User: " + super().__str__() + ' . Total matches: ' + str(self.total_matches) + ' . Wins: ' + str(self.wins) + ' . Losses: ' + str(self.losses)
