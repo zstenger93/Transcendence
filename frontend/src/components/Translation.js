913488185d6826dfa57d6d54c58a213f73247eb1
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-21 08:44:35 +0100

    added a button to choose language and json for hu

--- frontend/src/components/Translation.js
+++ frontend/src/components/Translation.js
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslations from "../locales/en/en.json";
 import frTranslations from "../locales/fr/fr.json";
+import huTranslations from "../locales/hu/hu.json";
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -11,10 +12,11 @@ i18n.use(initReactI18next).init({
     fr: {
       translation: frTranslations,
     },
-
-    // Add more language translations as needed
+    hu: {
+      translation: huTranslations,
+    },
   },
-  lng: "fr",
+  lng: "en",
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
