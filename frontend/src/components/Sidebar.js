b4137674e588572caf1bb141127a2181a537cae0
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-12-21 08:44:35 +0100

    added a button to choose language and json for hu

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { TbLogout } from "react-icons/tb";
 import { useTranslation } from "react-i18next";
-import { FaInfoCircle, FaHome, FaUser } from "react-icons/fa";
+import i18n from "i18next";
+import { FaInfoCircle, FaHome, FaUser, FaLanguage } from "react-icons/fa";
 import { SiGooglechat, SiGameandwatch } from "react-icons/si";
 
 const Sidebar = () => {
   const { t } = useTranslation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setDropdownOpen(false);
+  };
+
   return (
     <div
-      className="fixed top-0 lef-0 h-screen w-16 m-0
-				flex flex-col bg-gray-800 bg-opacity-30 text-white 
-				shadow-lg"
+      className="fixed top-0 left-0 h-screen w-16 m-0
+								flex flex-col bg-gray-800 bg-opacity-30 text-white 
+								shadow-lg"
     >
       <div className="mt-10 space-y-5">
         <SidebarIcon icon={<FaHome size="32" />} text={t("Home")} to="/home" />
@@ -35,6 +43,17 @@ const Sidebar = () => {
           text={t("About Us")}
           to="/about"
         />
+        <SidebarDropdown
+          icon={<FaLanguage size="32" />}
+          text={t("Language")}
+          options={[
+            { value: "en", label: "English" },
+            { value: "fr", label: "French" },
+            { value: "hu", label: "Hungarian" },
+            // Add more languages as needed
+          ]}
+          onSelect={changeLanguage}
+        />
       </div>
       <div className="mb-10 mt-auto">
         <SidebarIcon icon={<TbLogout size="32" />} text={t("Logout")} to="/" />
@@ -48,20 +67,67 @@ const SidebarIcon = ({ icon, text = "tooltip", to, margin }) => {
     <Link
       to={to}
       className={`relative flex items-center justify-center
-								h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
-								bg-purple-900 bg-opacity-50 text-blue-300 hover:bg-blue-300
-								hover:text-purple-900 rounded-3xl hover:rounded-xl transition-all
-								duration-300 ease-linear cursor-pointer group ${margin}`}
+					h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
+					bg-purple-900 bg-opacity-50 text-blue-300 hover:bg-blue-300
+					hover:text-purple-900 rounded-3xl hover:rounded-xl transition-all
+					duration-300 ease-linear cursor-pointer group ${margin}`}
     >
       {icon}
       <span
         className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md
-							text-white bg-gray-900 text-xs font-bold transition-all duration-100
-							scale-0 origin-left group-hover:scale-100"
+					text-white bg-gray-900 text-xs font-bold transition-all 
+					duration-100 scale-0 origin-left group-hover:scale-100"
       >
         {text}
       </span>
     </Link>
+  );
+};
+
+const SidebarDropdown = ({ icon, text, options, onSelect }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleSelect = (value) => {
+    onSelect(value);
+    setDropdownOpen(false);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        className={`relative flex items-center justify-center
+					h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
+					bg-purple-900 bg-opacity-50 text-blue-300 hover:bg-blue-300
+					hover:text-purple-900 rounded-3xl hover:rounded-xl transition-all
+					duration-300 ease-linear cursor-pointer group`}
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        {icon}
+        <span
+          className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md
+					text-white bg-gray-900 text-xs font-bold transition-all duration-100
+					scale-0 origin-left group-hover:scale-100"
+        >
+          {text}
+        </span>
+      </button>
+      {dropdownOpen && (
+        <div
+          className="absolute left-0 w-22 py-2 mt-2 bg-gray-900 bg-opacity-80
+					rounded-lg shadow-xl"
+        >
+          {options.map((option) => (
+            <button
+              key={option.value}
+              className="block px-4 py-2 text-gray-300 hover:bg-indigo-500 hover:text-white"
+              onClick={() => handleSelect(option.value)}
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
