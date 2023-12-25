1d8ec10f97167db9c29490eb1aa3f2336ef6189a
AzerSD <sioudazer8@gmail.com>
2023-12-25 07:20:19 +0100

    deleteing everything in the branch

--- backend/leaderboard/urls.py
+++
@@ -1,9 +0,0 @@
-# users_list/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('leaderboard/', views.leaderboard, name='leaderboard'),
-    # path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
-    # Add other URL patterns as needed
-]
