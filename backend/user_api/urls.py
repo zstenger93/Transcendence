29709b82473d95982c917c7ef26adc1ee671a1a0
AzerSD <sioudazer8@gmail.com>
2023-12-31 13:15:23 +0100

    removed the token endpoints

--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('oauth/authorize/', views.OAuthAuthorize.as_view(), name='oauth_authorize'),
     path('oauth/callback/', views.OAuthCallback.as_view(), name='oauth_callback'),
     path('users/', views.UserViewSet.as_view({'get': 'list'}), name='users'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
