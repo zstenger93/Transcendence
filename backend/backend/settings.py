2094cb6e6306dad9150e8a7b01894f9607cfd99c
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-05 12:05:55 +0100

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,9 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '10.13.7.5', 'transcendence-backend-znhl.onrender.com']
 
