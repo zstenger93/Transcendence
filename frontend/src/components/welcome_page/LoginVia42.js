208fa0fd56f4ed457ae2e1725528a567a0f91fcf
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-05 14:56:45 +0100

    commented out thrash oauth

--- frontend/src/components/welcome_page/LoginVia42.js
+++ frontend/src/components/welcome_page/LoginVia42.js
@@ -2,31 +2,31 @@ import React from "react";
 import { WelcomeButtonStyle } from "../buttons/ButtonStyle";
 
 const OAuth = async ({ navigate, redirect_uri }) => {
-	const auth = `${redirect_uri}/api/is_authenticated/`;
-	let response = await fetch(auth, {
-	  credentials: "include",
-	});
+	// const auth = `${redirect_uri}/api/is_authenticated/`;
+	// let response = await fetch(auth, {
+	//   credentials: "include",
+	// });
   
-	let data = await response.json();
+	// let data = await response.json();
   
-	if (!data.is_authenticated) {
-	  const authWindow = window.open(`${redirect_uri}/api/oauth/authorize/`);
-	  window.addEventListener(
-		"message",
-		(event) => {
-		  if (event.origin !== redirect_uri) {
-			return;
-		  }
-		  if (event.data.is_authenticated) {
-			authWindow.close();
-			navigate("/home");
-		  }
-		},
-		false
-	  );
-	} else {
+	// if (!data.is_authenticated) {
+	//   const authWindow = window.open(`${redirect_uri}/api/oauth/authorize/`);
+	//   window.addEventListener(
+	// 	"message",
+	// 	(event) => {
+	// 	  if (event.origin !== redirect_uri) {
+	// 		return;
+	// 	  }
+	// 	  if (event.data.is_authenticated) {
+	// 		authWindow.close();
+	// 		navigate("/home");
+	// 	  }
+	// 	},
+	// 	false
+	//   );
+	// } else {
 	  navigate("/home");
-	}
+	// }
   };
 
 const LoginButton = ({ t, navigate, redirect_uri }) => {
