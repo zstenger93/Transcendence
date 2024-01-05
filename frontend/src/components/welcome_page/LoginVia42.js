5078ec8f2cbb45234b62333967838e5aeccb86d7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-05 19:17:53 +0100

    Love timing issues with api

--- frontend/src/components/welcome_page/LoginVia42.js
+++ frontend/src/components/welcome_page/LoginVia42.js
@@ -2,32 +2,33 @@ import React from "react";
 import { WelcomeButtonStyle } from "../buttons/ButtonStyle";
 
 const OAuth = async ({ navigate, redirect_uri }) => {
-	// const auth = `${redirect_uri}/api/is_authenticated/`;
-	// let response = await fetch(auth, {
-	//   credentials: "include",
-	// });
-  
-	// let data = await response.json();
-  
-	// if (!data.is_authenticated) {
-	//   const authWindow = window.open(`${redirect_uri}/api/oauth/authorize/`);
-	//   window.addEventListener(
-	// 	"message",
-	// 	(event) => {
-	// 	  if (event.origin !== redirect_uri) {
-	// 		return;
-	// 	  }
-	// 	  if (event.data.is_authenticated) {
-	// 		authWindow.close();
-	// 		navigate("/home");
-	// 	  }
-	// 	},
-	// 	false
-	//   );
-	// } else {
-	  navigate("/home");
-	// }
-  };
+  const auth = `${redirect_uri}/api/is_authenticated/`;
+  let response = await fetch(auth, {
+    credentials: "include",
+  });
+
+  let data = await response.json();
+  if (!data.is_authenticated) {
+    const authWindow = window.open(`${redirect_uri}/api/oauth/authorize/`);
+    const checkAuthentication = async () => {
+      response = await fetch(auth, {
+        credentials: "include",
+      });
+      data = await response.json();
+      if (data.is_authenticated) {
+        console.log("authenticated");
+        navigate("/home");
+        authWindow.close();
+      } else {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        checkAuthentication();
+      }
+    };
+    checkAuthentication();
+  } else {
+    navigate("/home");
+  }
+};
 
 const LoginButton = ({ t, navigate, redirect_uri }) => {
   return (
