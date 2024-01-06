9ceb3f0076aa6d6375fd66c9815e20d729a49ccf
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-06 09:32:17 +0100

    print inmanage.py

--- backend/manage.py
+++ backend/manage.py
@@ -5,18 +5,20 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+	"""Run administrative tasks."""
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+	for key, value in os.environ.items():
+		print(f"{key}: {value}")
+	try:
+		from django.core.management import execute_from_command_line
+	except ImportError as exc:
+		raise ImportError(
+			"Couldn't import Django. Are you sure it's installed and "
+			"available on your PYTHONPATH environment variable? Did you "
+			"forget to activate a virtual environment?"
+		) from exc
+	execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
-    main()
+	main()
