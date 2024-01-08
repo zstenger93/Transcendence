043cadac238fa5a69bac0356f5b53314b6074323
AzerSD <sioudazer8@gmail.com>
2024-01-08 21:28:06 +0100

    added lsof and an alias and some requirements to the backend

--- docker/backend/backend.sh
+++ docker/backend/backend.sh
@@ -10,6 +10,9 @@ pip install -r requirements.txt
 echo 'source /app/backend/venv/bin/activate' >> /root/.bashrc
 
 echo "alias migrate='python manage.py makemigrations && python manage.py migrate'" >> /root/.bashrc
+echo "alias get='http --follow --timeout 6'" >> /root/.bashrc
 
 echo "Starting Django Server, Enjoy!"
 python /app/backend/manage.py runserver 0.0.0.0:8000
+
+# lsof -n -P -i TCP:8000 -s TCP:LISTEN
