7751acbe9d385b04346a590d88e8ca10ea900b57
AzerSD <sioudazer8@gmail.com>
2024-01-09 07:09:51 +0100

    did some cleanup, now nginx works but not in harmony with django

--- docker/backend/backend.sh
+++ docker/backend/backend.sh
@@ -29,6 +29,10 @@ gunicorn -c config/gunicorn/dev.py
 ############################################
 apt install nginx -y
 service nginx start
+service nginx status
+
+cp /app/docker/backend/config/nginx.conf /etc/nginx/nginx.conf
+cp /app/docker/backend/config/backend.conf /etc/nginx/sites-available/backend
 
 openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.pem -subj "/C=DE/CN=localhost"
 openssl x509 -outform pem -in /etc/ssl/localhost.pem -out /etc/ssl/localhost.crt
