4c949ba74eb9eee856c30d5e4585d535ed6fab12
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-09 14:58:15 +0100

    manual facepalm

--- frontend/src/pages/Games/ChoosePongMode.js
+++ frontend/src/pages/Games/ChoosePongMode.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, useNavigate } from "react-router-dom";
 import BackButton from "../../components/buttons/BackButton";
-import { ButtonStyle } from "../../components/buttons/ButtonStyle";
+import {
+  ButtonStyle,
+  WelcomeButtonStyle,
+} from "../../components/buttons/ButtonStyle";
+import backgroundimage from "../../images/pongbg.png";
 
 function ChoosePongMode() {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { t } = useTranslation();
 
@@ -12,16 +17,10 @@ function ChoosePongMode() {
     <div className="flex flex-col items-center justify-center h-screen">
       <div
         className="p-10 bg-gray-900 bg-opacity-80 rounded-xl shadow-xl
-	   border-r-2 border-b-2 border-purple-600"
+	   border-r-2 border-b-2 border-purple-600 "
       >
         <Link to="/originalpong" className={`mb-4 ${ButtonStyle}`}>
           {t("Original Pong")}
-        </Link>
-        <Link to="/multiplayer" className={`mb-4 ${ButtonStyle}`}>
-          {t("Multiplayer")}
-        </Link>
-        <Link to="/moddedpong" className={`mb-4 ${ButtonStyle}`}>
-          {t("Modded Pong")}
         </Link>
         <Link to="/pongai" className={`mb-4 ${ButtonStyle}`}>
           {t("Pong against AI")}
@@ -29,8 +28,36 @@ function ChoosePongMode() {
         <Link to="/pong3d" className={`mb-4 ${ButtonStyle}`}>
           {t("3D Pong")}
         </Link>
+        <Link to="/multiplayer" className={`mb-8 ${ButtonStyle}`}>
+          {t("Multiplayer")}
+        </Link>
+        <Link
+          onClick={(e) => {
+            e.preventDefault();
+            setIsOpen(true);
+          }}
+          className={`mb-4 ${ButtonStyle}`}
+        >
+          {t("How to play")}
+        </Link>
         <BackButton navigate={navigate} t={t} />
       </div>
+      {isOpen && (
+        <div
+          className="absolute inset-0 z-50 flex items-center justify-center bg-center bg-cover bg-no-repeat"
+          style={{ backgroundImage: `url(${backgroundimage})` }}
+        >
+          <div className="flex flex-col justify-between h-full">
+		  <div className="flex-grow"></div>
+            <button
+              onClick={() => setIsOpen(false)}
+              className={`mb-12 ${WelcomeButtonStyle}`}
+            >
+              {t("Back")}
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
