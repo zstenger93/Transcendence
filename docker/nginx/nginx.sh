23ad05fd52f5288d54a1dcde5c97b3a14a862a9e
AzerSD <sioudazer8@gmail.com>
2024-01-11 14:49:50 +0100

    moved nginx from the backend container to a separate container

---
+++ docker/nginx/nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "!!!!! Nginx Status !!!!!"
+service nginx status
+echo "!!!!! Nginx Status !!!!!"
+
+cp /app/docker/nginx/config/nginx.conf /etc/nginx/nginx.conf
+cp /app/docker/nginx/config/backend.conf /etc/nginx/sites-available/backend
+
+openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.pem -subj "/C=DE/CN=localhost"
+openssl x509 -outform pem -in /etc/ssl/localhost.pem -out /etc/ssl/localhost.crt
+
+cd /etc/nginx/sites-enabled
+ln -s ../sites-available/backend .
+
+service nginx restart
+service nginx status
