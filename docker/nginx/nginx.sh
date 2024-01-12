589073a3ffd4ff7f333b362a8979b91cb26cc4d9
AzerSD <sioudazer8@gmail.com>
2024-01-12 08:39:35 +0100

    fixed nginx container and dockercompose

--- docker/nginx/nginx.sh
+++ docker/nginx/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+service nginx start
 echo "!!!!! Nginx Status !!!!!"
 service nginx status
 echo "!!!!! Nginx Status !!!!!"
@@ -10,8 +10,9 @@ cp /app/docker/nginx/config/backend.conf /etc/nginx/sites-available/backend
 openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.pem -subj "/C=DE/CN=localhost"
 openssl x509 -outform pem -in /etc/ssl/localhost.pem -out /etc/ssl/localhost.crt
 
-cd /etc/nginx/sites-enabled
+cd /etc/nginx/sites-enabled && \
 ln -s ../sites-available/backend .
 
 service nginx restart
 service nginx status
+tail -f /dev/null
