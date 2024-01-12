ddf9573020d60e5d61a01c6d199d42e4d97a9e30
AzerSD <sioudazer8@gmail.com>
2024-01-12 10:37:29 +0100

    fixed frontend.conf

--- docker/nginx/nginx.sh
+++ docker/nginx/nginx.sh
@@ -4,12 +4,14 @@ service nginx status
 
 cp /app/docker/nginx/config/nginx.conf /etc/nginx/nginx.conf
 cp /app/docker/nginx/config/backend.conf /etc/nginx/sites-available/backend
+cp /app/docker/nginx/config/frontend.conf /etc/nginx/sites-available/frontend
 
 openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.pem -subj "/C=DE/CN=localhost"
 openssl x509 -outform pem -in /etc/ssl/localhost.pem -out /etc/ssl/localhost.crt
 
 cd /etc/nginx/sites-enabled
 ln -s ../sites-available/backend .
+ln -s ../sites-available/frontend .
 
 service nginx restart
 service nginx status
