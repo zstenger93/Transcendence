bb62b6d4b297dc2eb09bdea9792df3381a9e273c
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-13 07:48:54 +0100

    style: working on fade-in for text

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,6 +26,15 @@ module.exports = {
 	  borderRadius: {
         'custom': '6%',
       },
+	  animation: {
+        fadeIn: 'fadeIn 2s ease-in forwards'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
     },
   },
   plugins: [],
