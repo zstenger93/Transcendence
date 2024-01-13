c3a5beab2513b07bd93fb810e49681411db7029e
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-13 09:50:14 +0100

    style: added gradient to readme bg on home

--- frontend/src/components/home/Readme.js
+++ frontend/src/components/home/Readme.js
@@ -56,7 +56,7 @@ function Readme() {
       {isOpen && (
         <div
           className="fixed top-0 left-0 w-full h-full flex items-center
-			    justify-center bg-black bg-opacity-90 rounded-xl"
+			    justify-center bg-black bg-opacity-90"
         >
           <div className="">
             <button
