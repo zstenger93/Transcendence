053efb8ff4b95e2de12216c6046dd342fa425e61
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-13 09:50:14 +0100

    style: added gradient to readme bg on home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,12 +5,24 @@ import WelcomeMessage from "../components/home/WelcomeMessage";
 import Readme from "../components/home/Readme";
 import Sidebar from "../components/hamburger_menu/Hamburger";
 
+
+function ScrollDownIndicator() {
+	return (
+		<div className="absolute bottom-0 flex justify-center items-center h-24 w-full">
+			<span className="animate-bounce text-8xl text-orange-900 text-opacity-40">
+				&#8595;
+			</span>
+		</div>
+	);
+}
+
 function Home() {
 	return (
 		<>
 			<div className="relative h-screen flex items-center justify-center">
 				<WelcomeMessage />
-				<div className="absolute top-full w-full bg-black bg-opacity-50">
+				<ScrollDownIndicator />
+				<div className="absolute top-full w-full bg-black home-bg-black-gradient">
 					<Sidebar />
 					<Readme />
 				</div>
