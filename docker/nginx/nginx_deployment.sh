29f26258ed07e474a4de76fa33d8e0bd78beb459
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-01-16 08:07:25 +0100

    deployment script check

---
+++ docker/nginx/nginx_deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+service nginx status
+
+
+cp docker/nginx/config/nginx.conf /etc/nginx/nginx.conf
+cp docker/nginx/config/backend.conf /etc/nginx/sites-available/backend
+cp docker/nginx/config/frontend.conf /etc/nginx/sites-available/frontend
+
+sed -i 's|${FRONTEND_URL}|'${FRONTEND_URL}'|g' /etc/nginx/sites-available/backend
+sed -i 's|${BACKEND_URL}|'${BACKEND_URL}'|g' /etc/nginx/sites-available/backend
+
+openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /etc/ssl/localhost.key -out /etc/ssl/localhost.pem -subj "/C=DE/CN=localhost"
+openssl x509 -outform pem -in /etc/ssl/localhost.pem -out /etc/ssl/localhost.crt
+
+cd /etc/nginx/sites-enabled
+
+# PROTECTING AGAINST SYMLINK ATTACKS / ERRORS THAT IT ALREADY EXISTS
+if [ ! -e ./backend ]; then
+    ln -s ../sites-available/backend .
+fi
+
+service nginx restart
+service nginx status
+tail -f /dev/null
