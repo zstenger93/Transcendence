85d36299b4c2149088d6cdfd5164bba4889e388a
AzerSD <sioudazer8@gmail.com>
2024-01-22 12:00:50 +0100

    fixed

--- backend/user_api/urls.py
+++ backend/user_api/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     ## user_management
-	path('register/', views.UserRegister.as_view(), name='register'),
-	path('login/', views.UserLogin.as_view(), name='login'),
-	path('logout/', views.UserLogout.as_view(), name='logout'),
-	path('profile/', views.UserProfileView.as_view(), name='profile'),
+	path('register', views.UserRegister.as_view(), name='register'),
+	path('login', views.UserLogin.as_view(), name='login'),
+	path('logout', views.UserLogout.as_view(), name='logout'),
+	path('profile', views.UserProfileView.as_view(), name='profile'),
     path('oauth/authorize/', views.OAuthAuthorize.as_view(), name='oauth_authorize'),
     path('oauth/callback/', views.OAuthCallback.as_view(), name='oauth_callback'),
     path('users/', views.UserViewSet.as_view({'get': 'list'}), name='users'),
