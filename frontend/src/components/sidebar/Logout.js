349e50c079f19b2f0f845b6550f3446db34a95a9
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-02-07 11:30:54 +0100

    removed prints, unused stuff

--- frontend/src/components/sidebar/Logout.js
+++ frontend/src/components/sidebar/Logout.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 const userLogout = async ({ redirectUri }) => {
-	try {
-		const token = localStorage.getItem("access");
-		const response = await axios.post(
-			`${redirectUri}/api/logout`,
-			{},
-			{
-				withCredentials: true,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			}
-		);
-		localStorage.removeItem("refresh");
-		localStorage.removeItem("access");
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const token = localStorage.getItem("access");
+    await axios.post(
+      `${redirectUri}/api/logout`,
+      {},
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("access");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default userLogout;
