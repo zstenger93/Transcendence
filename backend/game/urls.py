9fc3a23b8b75182362ea50cb8b1c1660d24fc651
AzerSD <sioudazer8@gmail.com>
2024-03-16 06:22:29 +0100

    added game endpoints

---
+++ backend/game/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "game"
+
+urlpatterns = [
+    path("1v1/<str:other_user>/", views.room, name="room"),
+    path("ending/", views.game_ending, name="game_ending"),
+    path('send_push', views.send_push, name='send_push'),
+]
