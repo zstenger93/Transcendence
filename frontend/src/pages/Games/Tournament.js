68353968fcc948008b79c4c185d378ce91dec452
kvebers <karlisvilhelms@gmail.com>
2024-03-29 15:47:56 +0100

    Made Basic Layout of the canvas, changes In PongAI Center

--- frontend/src/pages/Games/Tournament.js
+++ frontend/src/pages/Games/Tournament.js
@@ -1,60 +1,114 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import backgroundImage from "../../images/pongbg.png";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import BackButton from "../../components/buttons/BackButton";
 import { useTranslation } from "react-i18next";
 import { WelcomeButtonStyle } from "../../components/buttons/ButtonStyle";
-import LoseScreen from "../../components/game/LoseScreen";
-import WinScreen from "../../components/game/WinScreen";
-import handleResize from "../../components/game/HandleResize";
-import FullScreenButton from "../../components/buttons/FullScreen";
 
-
-
-class player extends React.Component {
-    constructor(playerName, id) {
-        this.playerName = playerName;
-        this.id = id;
-    }
-    renderPlayer() {
-        return (
-            <div style={{width: "200px", height: "200px", border: "1px solid white"}}>
-            </div>
-        )
-    }
+function drawOnSelectionCanvas() {
+  const canvas = document.getElementById("selectionCanvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = canvas.width * 4;
+  canvas.height = canvas.height * 4;
+  ctx.fillRect(10, 10, 100, 50);
+  ctx.strokeText("Selection Page Content", 50, 30);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const Tournament = () => {
-    const navigate = useNavigate();
-    const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const [pageToRender, setPageToRender] = useState(0);
+  useEffect(() => {
+    if (pageToRender === 1) {
+      drawOnSelectionCanvas();
+    }
+  }, [pageToRender]);
+
+  function createTournomentButtonClicked() {
+    setPageToRender(1);
+  }
+
+  function startTournomentButtonPressed() {
+    setPageToRender(2);
+  }
+
+  function tournomentPage() {
     return (
-        <div className="flex justify-center items-center h-screen">
-        <div className="relative">
-        <img
-          src={backgroundImage}
-          style={{ width: "80vw", height: "100%", objectFit: "cover" }}
-          alt="Background"
-          className="rounded-xl shadow-lg"
-        />
-        <BackButton navigate={navigate} t={t} />
-      </div>
+      <div className="flex justify-center items-center h-screen">
+        <canvas
+          style={{
+            width: "80vw",
+            height: "45vw",
+            objectFit: "cover",
+            backgroundColor: "black",
+            border: "1px solid white",
+          }}
+        ></canvas>
       </div>
     );
+  }
+
+  function selectionPage() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative">
+          <canvas
+            id="selectionCanvas"
+            style={{
+              width: "80vw",
+              height: "45vw",
+              objectFit: "cover",
+              backgroundColor: "black",
+              border: "1px solid white",
+            }}
+          ></canvas>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2  -translate-y-1/2 text-center font-bold font-nosifer">
+            <button
+              onClick={startTournomentButtonPressed}
+              className={`mt-10 ${WelcomeButtonStyle}`}
+            >
+              {t("Start Tournament")}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  function startPage() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative">
+          <img
+            src={backgroundImage}
+            style={{ width: "80vw", height: "45vw", objectFit: "cover" }}
+            alt="Background"
+            className="rounded-xl shadow-lg"
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2  -translate-y-1/2 text-center font-bold font-nosifer">
+            <button
+              onClick={createTournomentButtonClicked}
+              className={`mt-10 ${WelcomeButtonStyle}`}
+            >
+              {t("Create Torunament")}
+            </button>
+          </div>
+          <BackButton navigate={navigate} t={t} />
+        </div>
+      </div>
+    );
+  }
+
+  switch (pageToRender) {
+    case 0:
+      return startPage();
+    case 1:
+      return selectionPage();
+    case 2:
+      return tournomentPage();
+    default:
+      return startPage();
+  }
 };
 
 export default Tournament;
