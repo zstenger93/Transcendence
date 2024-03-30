01ea272fc8446725cbf2043406758baf5b4390e0
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-03-30 14:42:42 +0100

    translations

--- frontend/src/components/profile/UserSettings.js
+++ frontend/src/components/profile/UserSettings.js
@@ -33,7 +33,7 @@ function UserSettings({ setProfilePicture, redirectUri }) {
     if (password === confirmPassword) {
       changePassword({ redirectUri, password });
     } else {
-      alert("Passwords do not match");
+      alert(t("Passwords do not match"));
     }
   };
 
@@ -125,7 +125,7 @@ function UserSettings({ setProfilePicture, redirectUri }) {
             />
           </div>
           <button className={`mx-auto block mb-2 ${ButtonStyle}`} type="submit">
-            Submit
+            {t("Submit")}
           </button>
         </form>
       )}
