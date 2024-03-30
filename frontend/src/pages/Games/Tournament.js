180c15810ab84bbd131e8ef7abd25cd18c7178e0
kvebers <karlisvilhelms@gmail.com>
2024-03-30 16:19:06 +0100

    Added Button On Click Events

--- frontend/src/pages/Games/Tournament.js
+++ frontend/src/pages/Games/Tournament.js
@@ -10,6 +10,7 @@ const Tournament = () => {
   const { t } = useTranslation();
   const [pageToRender, setPageToRender] = useState(0);
   const canvasRef = useRef(null);
+  let playerModeToAdd = 0;
 
   useEffect(() => {
     if (pageToRender === 1) {
@@ -28,6 +29,18 @@ const Tournament = () => {
   function drawOnSelectionCanvas() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
+  }
+
+  function selectButton(input) {
+    for (let i = 0; i < 4; i++) {
+      const button = document.getElementById("button" + i);
+      button.style.width = "12%";
+      button.style.height = "90%";
+    }
+    playerModeToAdd = input;
+    const selectedButton = document.getElementById("button" + input);
+    selectedButton.style.width = "14%";
+    selectedButton.style.height = "100%";
   }
 
   function tournomentPage() {
@@ -99,6 +112,56 @@ const Tournament = () => {
                   textIndent: "1vw",
                 }}
               ></input>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "20%",
+                  justifyContent: "center",
+                }}
+              >
+                <button
+                  onClick={() => selectButton(0)}
+                  id="button0"
+                  style={{
+                    width: "14%",
+                    height: "100%",
+                    margin: "3%",
+                    border: "1px solid white",
+                  }}
+                ></button>
+                <button
+                  onClick={() => selectButton(1)}
+                  id="button1"
+                  style={{
+                    width: "12%",
+                    height: "90%",
+                    margin: "3%",
+                    border: "1px solid white",
+                  }}
+                ></button>
+                <button
+                  onClick={() => selectButton(2)}
+                  id="button2"
+                  style={{
+                    width: "12%",
+                    height: "90%",
+                    margin: "3%",
+                    border: "1px solid white",
+                  }}
+                ></button>
+                <button
+                  onClick={() => selectButton(3)}
+                  id="button3"
+                  style={{
+                    width: "12%",
+                    height: "90%",
+                    margin: "3%",
+                    border: "1px solid white",
+                  }}
+                ></button>
+              </div>
               <button
                 style={{
                   width: "80%",
