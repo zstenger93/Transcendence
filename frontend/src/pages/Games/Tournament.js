64a64f648d1e89c8b5e9950eedec8efb04033ed5
kvebers <karlisvilhelms@gmail.com>
2024-03-30 15:08:18 +0100

    Added Input fields to add players to the game

--- frontend/src/pages/Games/Tournament.js
+++ frontend/src/pages/Games/Tournament.js
@@ -28,7 +28,6 @@ const Tournament = () => {
   function drawOnSelectionCanvas() {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-    
   }
 
   function tournomentPage() {
@@ -63,13 +62,58 @@ const Tournament = () => {
               border: "1px solid white",
             }}
           ></canvas>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2  -translate-y-1/2 text-center font-bold font-nosifer">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold font-nosifer">
             <button
               onClick={startTournomentButtonPressed}
               className={`mt-10 ${WelcomeButtonStyle}`}
             >
               {t("Start Tournament")}
             </button>
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              height: "100%",
+              width: "25%",
+              backgroundColor: "#8C8C8C",
+              transform: "translateY(-100%) translateX(300%)",
+              border: "1px solid white",
+            }}
+          >
+            <div
+              style={{
+                height: "30%",
+                width: "100%",
+                backgroundColor: "#BFBFBF",
+                border: "1px solid white",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <input
+                style={{
+                  height: "20%",
+                  width: "80%",
+                  margin: "5%",
+                  textIndent: "1vw",
+                }}
+              ></input>
+              <button
+                style={{
+                  width: "80%",
+                  height: "20%",
+                  backgroundColor: "#0D0D0D",
+                  border: "1px solid white",
+                  color: "white",
+                  margin: "5%",
+                  fontFamily: "Nosifer",
+                  fontSize: "1vw",
+                }}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
       </div>
